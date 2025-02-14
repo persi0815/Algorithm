13063af066a2d6f0428b6f6ccf4e7cf042905fbb
양지원 <121751509+persi0815@users.noreply.github.com>
2025-02-14 17:49:34 +0900

    [Silver IV] Title: 스케이트 연습, Time: 4 ms, Memory: 2020 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* 스케이트 연습
+도착 지점을 기준으로 1씩 증가하는 모습이 가능. 
+중간에 떨어졌다면, 거기서 1씩만 오를 수 있음!!
+
+*/
+
+int n;
+vector<int> arr(n);
+int r_sum = 0;
+int num = 1;
+
+void solution() {
+	for (int i = n - 1; i >= 0; i--) {
+		if (arr[i] > num) {
+			r_sum += num;
+			num++;
+		}
+		else {
+			r_sum += arr[i];
+			num = (arr[i] + 1);
+		}
+	}
+}
+
+int main() {
+	// 입력
+	cin >> n;
+	arr.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	// 로직
+	solution();
+
+	cout << r_sum; 
+
+	return 0;
+}
