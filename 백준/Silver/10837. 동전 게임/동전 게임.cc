3c39a06d4beae1f509aa40d5d39bbfa1d26866e4
양지원 <121751509+persi0815@users.noreply.github.com>
2025-04-06 20:54:53 +0900

    [Silver III] Title: 동전 게임, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.cc
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -1,92 +1,28 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <climits>
-#include <algorithm>
-#include <queue>
-#include <unordered_set>
-#include <tuple>
+#include <cmath>
 using namespace std;
 
-struct pair_hash {
-    size_t operator()(const pair<int, int>& p) const {
-        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-    }
-};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-int k, c; // 라운드 수, 입력의 개수
+    int k, c;
+    cin >> k >> c;
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    cin >> k >> c; 
+    while (c--) {
+        int a, b;
+        cin >> a >> b;
 
-    unordered_set<pair<int, int>, pair_hash> visited;
-    vector<tuple<int, int, int, bool>> ava; 
-    ava.push_back({0, 0, 0, 1});
-    visited.insert({0, 0});
+        int gap = abs(a - b);
+        int remain = k - max(a, b);
 
-    for(int r = 1; r <= k; r++){
-        int size_before = ava.size();
-        for(int s = size_before - 1; s >= 0; s--){
-            //if(get<0>(ava[s]) != r-1 || get<3>(ava[s]) == false) break;
-            if(get<0>(ava[s]) != r-1) break;
-            if(get<3>(ava[s]) == false) continue;
-
-            int yy = get<1>(ava[s]);
-            int dd = get<2>(ava[s]);
-
-            // 영희 앞면
-            if(yy + 1 + (k - r) >= dd) {
-
-                if (dd + 1 + (k - r) >= yy + 1) {
-                    if (visited.insert({yy + 1, dd + 1}).second)
-                        ava.push_back({r, yy + 1, dd + 1, true});
-                } else {
-                    if (visited.insert({yy + 1, dd + 1}).second)
-                        ava.push_back({r, yy + 1, dd + 1, false});
-                }
-
-                if (dd + (k - r) >= yy + 1) {
-                    if (visited.insert({yy + 1, dd}).second)
-                        ava.push_back({r, yy + 1, dd, true});
-                } else {
-                    if (visited.insert({yy + 1, dd}).second)
-                        ava.push_back({r, yy + 1, dd, false});
-                }
-                
-                
-            }
-
-            // 영희 뒷면
-            if(yy + (k - r) >= dd) {
-
-                if (dd + 1 + (k - r) >= yy) {
-                    if (visited.insert({yy, dd + 1}).second)
-                        ava.push_back({r, yy, dd + 1, true});
-                } else {
-                    if (visited.insert({yy, dd + 1}).second)
-                        ava.push_back({r, yy, dd + 1, false});
-                }
-
-                if (dd + (k - r) >= yy) {
-                    if (visited.insert({yy, dd}).second)
-                        ava.push_back({r, yy, dd, true});
-                } else {
-                    if (visited.insert({yy, dd}).second)
-                        ava.push_back({r, yy, dd, false});
-                }
-            }
+        if (a == b) {
+            cout << "1\n";
+        } else if (a < b) {
+            cout << ((gap - remain <= 1) ? "1\n" : "0\n");
+        } else {
+            cout << ((gap - remain <= 2) ? "1\n" : "0\n");
         }
-    }
-
-    // for(int i = 0; i < ava.size(); i++){
-    //     cout << get<0>(ava[i]) << " " << get<1>(ava[i]) << " " << get<2>(ava[i]) << " " << get<3>(ava[i]) << endl;
-    // }
-
-    for(int i = 0; i < c; i++){
-        int m, n; cin >> m >> n;
-        if (visited.count({m, n})) cout << "1\n";
-        else cout << "0\n";
     }
 
     return 0;
