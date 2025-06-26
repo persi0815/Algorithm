de187340590e4bae858110736090fbd08b9536c8
양지원 <121751509+persi0815@users.noreply.github.com>
2025-06-26 16:19:55 +0900

    [level 2] Title: 요격 시스템, Time: 117.97 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+폭격 미사일 요격. 미사일을 최소 사용하여 모든 폭격 미사일 요격하려함. 
+a 마사일은 x축에 평행한 직선 형태의 모양. 실수x 좌표에서도 발사 가능
+b는 y축에 수평인 마사일 발사. 해당 x좌표에 걸쳐있는 모든 미사일 관통 가능. 
+
+부분합 문제
+*/
+
+// 폭격 미사일의 x 좌표 목록
+int solution(vector<vector<int>> targets) {
+    int answer = 0;
+    
+    vector<pair<int, int>> spot; 
+    
+    for(auto t: targets){
+        spot.push_back({t[1], t[0]}); // 끝 점 기준 오름차순 
+    }
+    spot.push_back({100000001, 100000001});
+    sort(spot.begin(), spot.end()); 
+    
+    int end = -1;
+    for(auto s: spot){
+        if(end == -1) end = s.first;
+        else{
+            if(s.second >= end){
+                end = s.first; answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
