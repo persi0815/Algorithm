219fdc3c85b9ac8381aa1782731606ddf6c069e3
양지원 <121751509+persi0815@users.noreply.github.com>
2026-02-13 19:16:38 +0900

    [level 3] Title: 징검다리 건너기, Time: 4.89 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> stones, int k) {
+    int answer = 0;
+    // 모든 조약돌은 전체적으로 n을 뺐을때 0이하가 k번 연속으로 발생하는지 확인
+    int end = *max_element(stones.begin(), stones.end()); 
+    int start = 0;
+    while(start <= end){ // 10^3
+        int mid = (start+end)/2; 
+        int jump = 0;
+        bool avail = true;
+        for(int i = 0; i < stones.size(); i++){ // 200000
+            if(stones[i] - mid < 0){
+                jump++;
+                if(jump >= k) {
+                    avail = false; break;
+                }
+            }else{
+                jump = 0;
+            }
+        }
+        if(!avail) {
+            end = mid-1;
+        }else{
+            start = mid+1;
+            answer = mid;
+        }
+    }
+    
+    //  최대 몇 명까지 징검다리를 건널 수 있는지 return
+    return answer ;
+}
