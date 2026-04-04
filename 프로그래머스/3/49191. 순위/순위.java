88b0ecb1ad53ed218477e00550f577321bcb702d
양지원 <121751509+persi0815@users.noreply.github.com>
2026-04-04 21:10:03 +0900

    [level 3] Title: 순위, Time: 34.28 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,56 +1,59 @@
 import java.util.*; 
-/*
-n명의 권투 선수가 대회 참여. 1번부터 n번까지 번호 수령. 
-경기 결과로 선수 순위 매김.
-*/
 
 class Solution {
-    boolean[] visited = new boolean[101];
-    void dfs(Set<Integer> arr[], int to, int now){
-        List<Integer> copy = new ArrayList<>(arr[now]);
-        for(int num : copy){
-            if(visited[num]) continue; 
-            
-            visited[num] = true; 
-            arr[to].add(num);
-            dfs(arr, to, num);
-            
-        }
-    }
+    boolean[] visited;
     
-    // 선수의 수, 경기 결과
     public int solution(int n, int[][] results) {
-        
-        Set<Integer> win[] = new HashSet[n+1];
-        Set<Integer> lose[] = new HashSet[n+1];
-        for(int i = 0; i <= n; i++){
-            win[i] = new HashSet<>();
-            lose[i] = new HashSet<>();
-        }
-        
-        for(int[] res : results){
-            int w = res[0]; int l = res[1];
-            win[w].add(l);
-            lose[l].add(w);
-        }
-        
-        for(int i = 1; i <= n; i++){
-            Arrays.fill(visited, false);
-            visited[i] = true;
-            dfs(win, i, i);
-        }
-        for(int i = 1; i <= n; i++){
-            Arrays.fill(visited, false);
-            visited[i] = true;
-            dfs(lose, i, i);
-        }
-        
-        
         int answer = 0;
+        
+        // 자신을 이긴 선수들
+        Set<Integer>[] win = new HashSet[n+1];
+        for(int i = 0; i <= n; i++){
+            win[i] = new HashSet<>(); 
+        }
+        
+        // 자신에게 진 선수들
+        Set<Integer>[] lose = new HashSet[n+1];
+        for(int i = 0; i <= n; i++){
+            lose[i] = new HashSet<>(); 
+        }
+        
+        for(int[] r : results){
+            int w = r[0]; int l = r[1];
+            win[l].add(w);
+            lose[w].add(l);
+        }
+        
+        visited = new boolean[n+1]; 
+        
+        for(int i = 1; i <= n; i++){ // 100
+            bfs(win, i); // 4600
+            bfs(lose, i);
+        }
+        
+        
+        // 자신을 이긴 선수 + 자신에게 진 선수 = n-1이면 순위 매길 수 있는 것
         for(int i = 1; i <= n; i++){
             if(win[i].size() + lose[i].size() == n-1) answer++;
         }
         
-        return answer; // 정확하게 순위 매길 수 있는 선수의 수
+        // 정확하게 순위를 매길 수 있는 선수의 수
+        return answer;
+    }
+    
+    void bfs(Set<Integer>[] graph, int i){
+        Arrays.fill(visited, false);
+        Queue<Integer> q = new LinkedList<>(); 
+        q.offer(i); visited[i] = true;
+        while(!q.isEmpty()){
+            int m = q.poll(); 
+
+            for(int p : graph[m]){
+                if(visited[p]) continue;
+                visited[p] = true; 
+                q.offer(p);
+                graph[i].add(p);
+            }
+        }
     }
 }
