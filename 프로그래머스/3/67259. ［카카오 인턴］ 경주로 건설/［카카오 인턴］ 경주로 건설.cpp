d979fd33c87ebf8ee5c4dc77f14e6b37a06793de
양지원 <121751509+persi0815@users.noreply.github.com>
2026-05-30 17:49:29 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.27 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -2,52 +2,51 @@
 #include <vector>
 #include <queue>
 #include <climits>
+#include <algorithm> // min_element
 
 using namespace std;
-vector<vector<int>> dyx = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; 
+
+vector<pair<int, int>> dyx = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // RDLU
+
 int solution(vector<vector<int>> board) {
-    int answer = INT_MAX;
-    int n = board.size();
-    int score[n][n][4];// 해당 칸까지 가는데에 드는 비용
+    int answer = 0;
+    int n = board.size(); 
+    
+    // (0, 0) -> (N-1, N-1) 까지 BFS. 각 칸의 최소 비용 cost에 적재!
+    int c[n][n][4]; 
+    queue< pair< pair<int, int>, pair<int, int> > > q; // {{y, x}, {dir, cost}} 
+    
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            for(int d = 0; d < 4; d++){
-                score[i][j][d] = INT_MAX;
+            for(int k = 0; k < 4; k++){
+                c[i][j][k] = INT_MAX; 
             }
         }
     }
-    for(int i = 0; i < 4; i++){
-        score[0][0][i] = 0; 
-    }
+    q.push({{0, 0}, {1, 0}}); q.push({{0, 0}, {0, 0}}); 
+    c[0][0][0] = 0; c[0][0][1] = 0; c[0][0][2] = 0; c[0][0][3] = 0; 
     
-    // 해당칸 까지 가는데에 소요되는 비용 계산
-    queue<pair<pair<int, int>, int>> q; 
-    // 동, 남 확인하고 넣기
-    if(board[0][1] == 0) q.push({{0, 0}, 0});
-    if(board[1][0] == 0) q.push({{0, 0}, 1});
     while(!q.empty()){
         auto f = q.front(); q.pop(); 
         int y = f.first.first; int x = f.first.second; 
-        int dir = f.second; // 동서남북
+        int dir = f.second.first; int cost = f.second.second; 
         
-        for(int i = 0; i < 4; i++){
-            int ny = y + dyx[i][0]; 
-            int nx = x + dyx[i][1]; 
-            if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-            if(board[ny][nx] == 1) continue;
-            
-            int s = ((dir+i)%2)==0 ? score[y][x][dir] + 100 : score[y][x][dir] + 600;
-            if(score[ny][nx][i] > s){ // 기존 값보다 더 싼 값으로 갈 수 있으면
-                score[ny][nx][i] = s; // 점수 갱신
-                q.push({{ny, nx}, i});
+        for(int d = 0; d < 4; d++){
+            int ny = y + dyx[d].first; 
+            int nx = x + dyx[d].second; 
+            if(ny >= n || nx >= n || ny < 0 || nx < 0) continue; 
+            if(board[ny][nx] == 1) continue; // 벽
+            if((dir+2)%4 == d) continue; // 거꾸로
+            if(dir == d && c[ny][nx][d] > (cost + 100)){ // 직전
+                c[ny][nx][d] = cost + 100; 
+                q.push({{ny, nx}, {d, cost+100}}); 
+            } else if(dir != d && c[ny][nx][d] > (cost + 600)){ // 꺽기
+                c[ny][nx][d] = cost + 600; 
+                q.push({{ny, nx}, {d, cost+600}});
             }
         }
     }
     
-    
-    // 견적서 작성을 위해 경주로를 건설하는 데 필요한 최소 비용
-    for(int i = 0; i < 4; i++){
-        answer = min(answer, score[n-1][n-1][i]);
-    }
-    return answer ;
+    // 경주로를 건설하는 데 필요한 최소 비용
+    return answer = *min_element(c[n-1][n-1], c[n-1][n-1]+4);
 }
