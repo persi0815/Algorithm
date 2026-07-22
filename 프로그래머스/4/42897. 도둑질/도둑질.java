70acc7daf3abb3cc74c5786beb8582d095b8dbc4
양지원 <121751509+persi0815@users.noreply.github.com>
2026-07-22 14:20:16 +0900

    [level 4] Title: 도둑질, Time: 19.93 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,56 +1,26 @@
 import java.util.*; 
-/*
-dp[선택 유무][i까지] = 최댓값
-*/
 
 class Solution {
     
-    // 집에 있는 돈
     public int solution(int[] money) {
-        int cnt = money.length;
+        int siz = money.length;
+        int[] firstO = new int[siz]; firstO[0] = money[0]; firstO[1] = Math.max(firstO[0], money[1]);
+        int[] firstX = new int[siz]; firstX[1] = money[1]; firstX[2] = Math.max(firstX[1], money[2]);
         
-        int[][] dp = new int[2][cnt];
-        int[][] dp_n = new int[2][cnt];
-        
-        Arrays.fill(dp[0], 0); // 0, 2 
-        Arrays.fill(dp[1], 0); // 1
-        
-        dp[1][0] = money[0]; dp[1][1] = money[1]; dp[1][2] = dp[1][0] + money[2];
-        dp[0][0] = 0; dp[0][1] = dp[1][0]; dp[0][2] = dp[1][1]; 
-        
-        dp_n[1][0] = 0; dp_n[1][1] = money[1]; dp_n[1][2] = money[2];
-        dp_n[0][0] = 0; dp_n[0][1] = 0; dp_n[0][2] = money[1];
-        
-        if (cnt == 3) {
-            return Math.max(
-                Math.max(dp_n[1][2], dp_n[0][2]),
-                Math.max(dp[1][1], dp[0][2])
+        for(int i = 2; i < siz-1; i++){
+            firstO[i] = Math.max(
+                firstO[i-1], 
+                firstO[i-2] + money[i]
             );
         }
-
-        for(int i = 3; i < cnt; i++){
-            if(i == cnt-1){ // 마지막이면 1번째꺼 털었었으면 마지막꺼 털면 안됨. 
-                dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]); 
-            }
-            else{
-                dp[1][i] = Math.max(dp[1][i-2], dp[1][i-3]) + money[i];
-                dp[1][i] = Math.max(dp[0][i-1] + money[i], dp[1][i]);
-                
-                dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]);
-                dp[0][i] = Math.max(dp[0][i], dp[0][i-1]);
-            }
-            
-            dp_n[1][i] = Math.max(dp_n[1][i-2], dp_n[1][i-3]) + money[i];
-            dp_n[1][i] = Math.max(dp_n[0][i-1] + money[i], dp_n[1][i]);
-
-            dp_n[0][i] = Math.max(dp_n[1][i-1], dp_n[1][i-2]);
-            dp_n[0][i] = Math.max(dp_n[0][i], dp_n[0][i-1]);
+        for(int i = 3; i < siz; i++){
+            firstX[i] += Math.max(
+                firstX[i-1], 
+                firstX[i-2] + money[i]
+            );
         }
         
-        return Math.max(
-                Math.max(dp_n[0][cnt-1], dp_n[1][cnt-1]),
-                dp[0][cnt-1]
-            );
-
+        
+        return Math.max(firstO[siz-2], firstX[siz-1]);
     }
 }
