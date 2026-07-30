3af6c1cb320e0a83696a83a034d19738831cbe05
양지원 <121751509+persi0815@users.noreply.github.com>
2026-07-30 11:55:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 61.64 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 121 MB, 시간: 120.56 ms
+메모리: 139 MB, 시간: 61.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 15일 15:21:58
+2026년 07월 30일 11:54:56
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,83 +1,83 @@
 import java.util.*; 
-/*
-모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return
-빈 큐에서 데이터 삭제하라는 연산이 주어질 경우, 해당 연산 무시
-
-값 넣을때 두개의 힙에 모두 데이터 넣고 map 갱신, 뺄때 map 보고 가능한지 판단 후 heap에서 제거
-*/
 
 class Solution {
-    public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        int num = operations.length; 
+    public int[] solution(String[] operations) { // 1,000,000 
         
-        // {원소, 개수}
-        Map<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> nums = new HashMap<>(); 
         
-        // 최솟값 삭제하기 위해
-        PriorityQueue<Integer> min_heap = new PriorityQueue<>();
-        // 최댓값 삭제하기 위해
-        PriorityQueue<Integer> max_heap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> {
+            return Integer.compare(b, a);
+        }); 
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> {
+            return Integer.compare(a, b);
+        }); 
         
-        // 명령어 수행
-        for(int i = 0; i < num; i++){
-            // System.out.println(i);
-            String[] words = operations[i].split(" ");
-            String order = words[0];
-            Integer number = Integer.parseInt(words[1]);
-            if("I".equals(order)){ // 삽입
-                map.merge(number, 1, Integer::sum);
-                min_heap.offer(number);
-                max_heap.offer(number);
+        for(String operation : operations){
+            String[] order = operation.split(" ");
+            if(order[0].equals("I")){ // 삽입
+                int n = Integer.parseInt(order[1]); 
+                // System.out.println("insert: " + n);
+                nums.merge(n, 1, Integer::sum);
+                minHeap.offer(n); maxHeap.offer(n);
                 
-                // System.out.println(map);
-                // System.out.println(max_heap);
-                // System.out.println(min_heap);
-            }else if("D".equals(order)){ // 제거
-                if(map.isEmpty()) continue;
-                if(number.equals(1)){ // 최댓값 삭제
-                    while(true){
-                        int key = max_heap.poll();
-                        if(map.containsKey(key)){
-                            // 하나 줄이기
-                            map.merge(key, -1, Integer::sum);
-                            // value가 0 되었다면 삭제
-                            if(map.get(key) == 0) map.remove(key);
-                            // 성공했다면 나가기
-                            break;
-                        }
-                    }
-                }else if(number.equals(-1)){ // 최솟값 삭제
-                    while(true){
-                        int key = min_heap.poll();
-                        if(map.containsKey(key)){
-                            // 하나 줄이기
-                            map.merge(key, -1, Integer::sum);
-                            // value가 0 되었다면 삭제
-                            if(map.get(key) == 0) map.remove(key);
-                            // 성공했다면 나가기
-                            break;
-                        }
-                    }
+            }else if(order[0].equals("D")){
+                if(order[1].equals("1")){ // 최댓값 삭제              
+                    deleteMax(maxHeap,nums);
+                }else{ // 최솟값 삭제
+                    deleteMin(minHeap, nums);
+
                 }
-                // System.out.println(map);
-                // System.out.println(max_heap);
-                // System.out.println(min_heap);        
             }
-        } // 명령어 수행 끝
-        
-        // map의 key 추출해서 정렬
-        List<Integer> keys = new ArrayList<>(map.keySet());
-        Collections.sort(keys); // 오름차순 정렬
-        
-        if(keys.isEmpty()){
-            answer[0] = 0;
-            answer[1] = 0;
-        }else{
-            answer[1] = keys.get(0);
-            answer[0] = keys.get(keys.size()-1);
         }
         
-        return answer; // 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]
+        // 모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return
+        int min = 0; int max = 0;
+        
+        String m = deleteMax(maxHeap, nums); 
+        if(!m.equals("-")) max = Integer.parseInt(m);
+        minHeap.offer(max); nums.merge(max, 1, Integer::sum);
+        
+        m = deleteMin(minHeap, nums); 
+        if(!m.equals("-")) min = Integer.parseInt(m);
+        
+        return new int[]{max, min};
+    }
+    
+    public String deleteMax(PriorityQueue<Integer> maxHeap, HashMap<Integer, Integer> nums){
+        int max = 0; boolean avail = false;
+        while(maxHeap.size() != 0) { 
+            // 이제 heap에 데이터가 없음
+            if(maxHeap.size() == 0) break;
+            // 데이터 뽑기
+            max = maxHeap.poll();
+            // 데이터가 이미 뽑혔음
+            if(!nums.containsKey(max) || (nums.containsKey(max) && nums.get(max) <= 0)) continue;
+            // 뽑혔으니 데이터 제거
+            nums.merge(max, -1, Integer::sum);
+            // System.out.println("del: " + max);
+            avail = true;
+            break;
+        }
+        if(avail) return String.valueOf(max);
+        else return "-";
+        
+    }
+    public String deleteMin(PriorityQueue<Integer> minHeap, HashMap<Integer, Integer> nums){
+        int min = 0; boolean avail = false;
+        while(minHeap.size() != 0) { 
+            // 이제 heap에 데이터가 없음
+            if(minHeap.size() == 0) break;
+            // 데이터 뽑기
+            min = minHeap.poll();
+            // 데이터가 이미 뽑혔음
+            if(!nums.containsKey(min) || (nums.containsKey(min) && nums.get(min) <= 0)) continue;
+            // 뽑혔으니 데이터 제거
+            nums.merge(min, -1, Integer::sum);
+            // System.out.println("del: " + min);
+            avail = true;
+            break;
+        }
+        if(avail) return String.valueOf(min);
+        else return "-";
     }
 }
