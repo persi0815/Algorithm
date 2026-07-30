7b8d7bc723756d156c13233a2a8ec9958cea226f
양지원 <121751509+persi0815@users.noreply.github.com>
2026-07-30 16:57:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.49 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,44 +1,54 @@
-import java.util.*; 
-/*
-여러 대가 강을 가로지를 때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아야 함. 
-다리에는 최대 bridge_length대까지 올라갈 수 있음. 다리는 weight 이하까지의 무게를 견딜 수 있음. 
+import java.util.*;
 
-현재 다리를 건너고 있는 트럭들을 투 포인터로 관리, 무게 관리 -> 다음 트럭 지날 수 있는지 판단
-*/
-
+// 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는가
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int truckNum = truck_weights.length;
-        int time = 0; // 모든 트럭이 다리를 건너는데 걸리는 시간
-        
-        // 초기화
-        int next_idx = 0; // 다음에 지나갈 트럭 인덱스
-        int load = 0;
-        Queue<Integer> bridge = new LinkedList<>(); 
-        for(int i = 0; i < bridge_length; i++){
-            bridge.add(0); 
+    
+    class Truck{
+        int weight; int approach; 
+        public Truck(int weight, int approach){
+            this.weight = weight; this.approach = approach; 
         }
+    }
+    
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
         
-        // 트럭 이동시키기
-        while(next_idx < truckNum){
+        // 정해진 순서대로 다리 건너기
+        Queue<Truck> q = new ArrayDeque<>(); 
+        int total_weight = 0; // 다리에 있는 트럭 총 무게 
+        int cur_time = 1;  // 현재 시각
+        
+        for(int t = 0; t < truck_weights.length; t++){ 
+            int truck = truck_weights[t]; // 해당 트럭 다리에 실을 것
             
-            int next_truck = truck_weights[next_idx];
-            if(next_truck + load - bridge.peek() <= weight){ // 올릴 수 있다면
-                load = load + next_truck - bridge.peek();
-                bridge.poll(); 
-                bridge.add(next_truck);
-                next_idx++;
-            }else{ // 올릴 수 없음
-                load = load - bridge.peek();
-                bridge.poll(); 
-                bridge.add(0);
+            // 시간 지난거 내리기
+            if(!q.isEmpty() && (q.peek().approach == (cur_time - bridge_length))) {
+                Truck bef = q.poll();
+                total_weight -= bef.weight;  // 물건 내리기
             }
             
-            time++;
+            // 무게가 한계에 도달했다면 -> 시간 흘려보내면서 트럭들 내리기
+            while(!q.isEmpty() && weight < (total_weight + truck)){
+                // 아직 내릴 시간이 안됨
+                if(q.peek().approach > (cur_time - bridge_length)){
+                    cur_time = q.peek().approach + bridge_length; // 내릴 수 있는 시간으로 만들어버리기
+                }
+                // 내릴 시간이 됨.
+                Truck bef = q.poll(); 
+                total_weight -= bef.weight;  // 물건 내리기
+            }
             
+            // 해당 차 큐에 넣기
+            q.offer(new Truck(truck, cur_time)); 
+            total_weight += truck;
+            answer = cur_time + bridge_length;
+            
+            
+            // 다음 시간으로
+            cur_time++;
         }
         
-        
-        return time + bridge_length;
+        // 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return
+        return answer;
     }
 }
