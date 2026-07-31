8931a6c9e64f5e77b90540cebb9166bdda6a3d2b
양지원 <121751509+persi0815@users.noreply.github.com>
2026-07-31 13:37:35 +0900

    [level 3] Title: 여행경로, Time: 1.55 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,40 +1,54 @@
 import java.util.*; 
-/*
-주어진 항공권을 '모두' 이용하여 여행경로를 짜려고 함. ICN 공항에서 출발
-단뱡향 이동. 
-경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 리턴  -> 연결리스트에서 정렬
-*/
 
 class Solution {
     
-    boolean[] visited;
-    List<String> path = new ArrayList<>();
+    boolean[] ticketUsed; 
+    List<String> path;
+    
+    public String[] solution(String[][] tickets) { // 항공권 정보
+        
+        path = new ArrayList<>(); 
+        ticketUsed = new boolean[tickets.length]; 
+        
+        // 각 list 알파벳 순으로 정렬
+        Arrays.sort(tickets, (a, b) -> {
+            if(!a[0].equals(b[0])) return (a[0]).compareTo(b[0]); 
+            else return (a[1]).compareTo(b[1]); 
+        });
 
-    public List<String> solution(String[][] tickets) { // String[]
-        Arrays.sort(tickets, (a, b) -> a[0].equals(b[0]) ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
-        visited = new boolean[tickets.length];
+        // 경로가 중요하니 dfs
         path.add("ICN");
-        dfs("ICN", tickets, 0);
-        return path;
-        //return answer.toArray(new String[0]); // Object[]를 String[]로 바꿔줘야 함. 
+        dfs("ICN", tickets);
+        
+        return path.stream().toArray(String[]::new);
     }
     
-    public boolean dfs(String curr, String[][] tickets, int used) {
-        if (used == tickets.length) {
-            return true; // 사전순 가장 빠른 경로를 찾으면 바로 종료
+    public boolean dfs(String cur, String[][] tickets){
+        // 티켓 전부 돌았다. 알파벳 순으로 가장 앞선 순서부터 방문 예정. 
+        if(path.size() == tickets.length+1){
+            return true;
         }
-
-        for (int i = 0; i < tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(curr)) {
-                visited[i] = true;
-                path.add(tickets[i][1]);
-                if (dfs(tickets[i][1], tickets, used + 1)) return true;
-                path.remove(path.size() - 1);
-                visited[i] = false;
-            }
+        // 다음 장소로 이동
+        for(int t = 0; t < tickets.length; t++){
+            String[] ticket = tickets[t]; 
+            
+            // 현재 출발지가 맞아야 함
+            if(!ticket[0].equals(cur)) continue; 
+            
+            // 이미 사용했다면 사용하지 못함
+            if(ticketUsed[t]) continue; 
+            
+            // 티켓 사용
+            ticketUsed[t] = true; 
+            path.add(ticket[1]); 
+            
+            // 다음 방문지 찾기
+            if(dfs(ticket[1], tickets)) return true; 
+            
+            // 원상 복구
+            path.remove(path.size()-1);
+            ticketUsed[t] = false; 
         }
-
         return false;
     }
-    
 }
